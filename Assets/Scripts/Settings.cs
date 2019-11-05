06db1afad3bf90099d29a04f8d3bb6e9ef051930
Alexandr Nitsenko <alexxnitsenko@gmail.com>
2019-11-05 12:57:25 +0300

    first

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+public class Settings : MonoBehaviour {
+
+
+    public AudioMixer am;
+    public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("masterVolume", sliderValue);
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/sphereBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphereBehavior : MonoBehaviour
+{
+
+    private Rigidbody rb; // Объявление новой переменной Rigidbody
+    private bool isMovingLeft = true; // переменная, отражающая условное направление объекта
+    private float speed = 250f; // Скорость движения объекта
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Получение доступа к Rigidbody
+    }
+
+    void changeDirection()
+    {
+        if (isMovingLeft)
+        {
+            isMovingLeft = false;
+        }
+        else
+        {
+            isMovingLeft = true;
+        }
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            changeDirection();
+        }
+
+        if (isMovingLeft)
+        {
+            rb.velocity = new Vector3(speed* Time.deltaTime, 0f, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, 0f, -speed* Time.deltaTime);
+        }
+
+    }
+
+}
